05aa1b5957b5c2f72b1e689a27e4d2e1df48698b
Hohlov <e.hohlov@kfins.ru>
2018-04-07 18:51:06 +0300

    Initial commit

---
+++ test01.py
@@ -0,0 +1,28 @@
+# a = 5
+# b = 10
+#
+# if a > b:
+#     print("A больше B")
+#     print(a - b)
+# else:
+#     print("B больше или равно A")
+#     print(b - a)
+#
+# print("The End")
+
+
+test = "Hello"
+test2 = "Eugene"
+
+test3 = test + " " + test2
+test4 = test3 + "1"
+
+if test3 == test + " " + test2 + " ":
+    print(test3)
+else:
+    print("NONO")
+    print(test3)
+    print(5+5)
+    print(id(test3))
+    print(id(test4))
+
